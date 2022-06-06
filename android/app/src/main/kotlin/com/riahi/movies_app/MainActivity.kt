a24e0bd97df7dae01e6d336b8c5f3af9fba3af4f
Anas Riahi <anasoasis41@gmail.com>
2022-06-06 02:22:41 +0100

    Create project and mockup app

---
+++ android/app/src/main/kotlin/com/riahi/movies_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.riahi.movies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/ui/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
